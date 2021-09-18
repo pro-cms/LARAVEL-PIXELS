8f84f6b202ddfa59e7651ddbac53fd39e0ae6480
Novath Thomas <57701433+pro-cms@users.noreply.github.com>
2021-09-18 22:22:58 +0300

    initial commit

---
+++ marketingtool/resources/views/admin/layout/master.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Dashboard</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="{{ asset('assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/vendors/themify-icons/css/themify-icons.css') }}" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="{{ asset('assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="{{ asset('assets/css/main.min.css') }}" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+</head>
+
+<body class="fixed-navbar">
+    <div class="page-wrapper">
+        <!-- START HEADER-->
+        @include('admin.partials.nav')
+        <!-- END HEADER-->
+        <!-- START SIDEBAR-->
+       @include("admin.partials.sidebar")
+        <!-- END SIDEBAR-->
+        <div class="content-wrapper">
+            <!-- START PAGE CONTENT-->
+            <div class="page-content fade-in-up">
+                    @yield('content')
+            </div>
+            <!-- END PAGE CONTENT-->
+           @include('admin.partials.footer')
+        </div>
+    </div>
+    <!-- BEGIN THEME CONFIG PANEL-->
+
+    <!-- END THEME CONFIG PANEL-->
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    {{-- <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div> --}}
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS-->
+    <script src="{{ asset('assets/vendors/jquery/dist/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/popper.js/dist/umd/popper.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/bootstrap/dist/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/metisMenu/dist/metisMenu.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+
+    <!-- CORE SCRIPTS-->
+    <script src="{{ asset('assets/js/app.min.js') }}" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="{{ asset('assets/js/scripts/dashboard_1_demo.js') }}" type="text/javascript"></script>
+</body>
+
+</html>
